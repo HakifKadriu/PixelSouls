13930f91195676765593785a5f2183ae88a4454f
Kifaa <hakifkadriu2003@gmail.com>
2024-08-02 22:02:12 +0200

    Small Changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    build: {
-        outDir: 'dist',
-    },
+  server: {
+    port: 5173, // Default port for Vite
+  },
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
 });
